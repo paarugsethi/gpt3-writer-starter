16faf50f61c1f465554088c4a99bf74b697aa158
Paarug Sethi <paarugsethi@gmail.com>
2023-06-24 15:12:53 +0530

    new try catch

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,34 +1,36 @@
 import { Configuration, OpenAIApi } from 'openai';
 
-
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = "Write a detailed email in a formal writing style. Include the recipient's email address, subject and body of the email. The email is to be written to ";
-const basePromptPrefix2 = ". The email is about "
+const basePromptPrefix =
+  "Write a detailed email in a formal writing style. Include the recipient's email address, subject and body of the email. The email is to be written to ";
+const basePromptPrefix2 = '. The email is about ';
 
-try{
 const generateAction = async (req, res) => {
+  try {
     // RUN FIRST PROMPT
-    console.log(`API: ${basePromptPrefix}${req.body.recipient}${basePromptPrefix2}${req.body.userInput}`)
+    console.log(
+      `API: ${basePromptPrefix}${req.body.recipient}${basePromptPrefix2}${req.body.userInput}`
+    );
 
     const baseCompletion = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `${basePromptPrefix}${req.body.recipient}${basePromptPrefix2}${req.body.userInput}\n`,
-        temperature: 0.7,
-        max_tokens: 250,
-    })
+      model: 'text-davinci-003',
+      prompt: `${basePromptPrefix}${req.body.recipient}${basePromptPrefix2}${req.body.userInput}\n`,
+      temperature: 0.7,
+      max_tokens: 250,
+    });
 
     const basePromptOutput = baseCompletion.data.choices.pop();
 
-    res.status(200).json({output: basePromptOutput});
+    res.status(200).json({ output: basePromptOutput });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err });
+  }
 };
-}
-catch (err){
-  console.log(err);
-}
 
 export default generateAction;
